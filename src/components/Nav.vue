49861e5b3ff117b0a269be44ddf0c9d70a85b7ab
duwei <1069646435@qq.com>
2021-03-19 16:51:58 +0800

    添加 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="nav">
-    <router-link to="/money">
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       记账
     </router-link>
     |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="label"/>
       标签
     </router-link>
     |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -18,8 +18,6 @@
 </template>
 
 <script lang="ts">
-
-
 
 
 //单引
@@ -33,6 +31,23 @@ export default {
 
 <style lang="scss" scoped>
 .nav {
-  border: 1px solid red;
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, .25);
+  flex-direction: row;
+  font-size: 12px;
+
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
