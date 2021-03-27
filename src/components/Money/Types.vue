c604acbbbe6c3d61ef0672c5fcd68bcc11146962
duwei <1069646435@qq.com>
2021-03-27 16:21:55 +0800

    使用 ts 实现了一个组件分支

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,28 +11,41 @@
   </div>
 </template>
 
-<script lang="js">
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
 
-export default {
-  name: 'Types',
-  props:['xxx'],
-  data() {
-    return {
-      type: '-'//'-'表示支出，'+'表示收入
+@Component
+export default class Types extends Vue {
+  type = '-';//'-'表示支出，'+'表示收入
+  selectType(type: string) { // type只能是'-'和'+'中的一个
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  },
-  mounted(){
-    console.log(this.xxx)
-  },
-  methods: {
-    selectType(type) { // type只能是'-'和'+'中的一个
-      if (type !== '-' && type !== '+') {
-        throw new Error('type is unknown')
-      }
-      this.type = type
-    }
+    this.type = type;
   }
 }
+
+// export default {
+//   name: 'Types',
+//   props:['xxx'],
+//   data() {
+//     return {
+//       type: '-'//'-'表示支出，'+'表示收入
+//     }
+//   },
+//   mounted(){
+//     console.log(this.xxx)
+//   },
+//   methods: {
+//     selectType(type) { // type只能是'-'和'+'中的一个
+//       if (type !== '-' && type !== '+') {
+//         throw new Error('type is unknown')
+//       }
+//       this.type = type
+//     }
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
