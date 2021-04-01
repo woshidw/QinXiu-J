4689748800f974582d3abd03ab60f902843c359b
duwei <1069646435@qq.com>
2021-04-01 22:00:31 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -27,17 +27,12 @@ tagListModel.fetch();
 
 @Component
 export default class Labels extends Vue {
-  tags = window.tagList;
+  tags = window.tagList; // 1
 
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (name) {
-      const message = tagListModel.create(name);
-      if (message === 'duplicated') {
-        window.alert('标签名重复了');
-      } else if (message === 'success') {
-        window.alert('添加成功');
-      }
+    if(name){
+      window.createTag(name);
     }
   }
 }
