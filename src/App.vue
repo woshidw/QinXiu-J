e4258b9a6c22624850ed868a0ac7f100a736201a
duwei <1069646435@qq.com>
2021-04-05 22:00:16 +0800

    store.count 有 bug 怎么办

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,29 @@
   </div>
 </template>
 
+<script lang="js">
+import store from '@/store/index2'
+
+export default {
+  data() {
+    return {
+      store: store
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import "~@/assets/style/helper.scss";
 @import "~@/assets/style/reset.scss";
 
-body{
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   line-height: 1.5;
   font-family: $font-hei;
   font-size: 16px;
-  background: #f5f5f5;
+  background: #F5F5F5;
 }
 </style>
--- src/store/index2.ts
+++ src/store/index2.ts
@@ -3,6 +3,10 @@ import tagStore from '@/store/tagStore';
 
 
 const store = {
+  count: 0,
+  addCount() {
+    this.count += 1;
+  },
   ...recordStore,
   ...tagStore,
 
