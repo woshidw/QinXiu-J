d3f3daac4d7e7a9ac89375db9d79e1bd728a72c1
duwei <1069646435@qq.com>
2021-04-07 15:52:35 +0800

    删除 Types.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -28,7 +28,6 @@
 
 <script lang="ts">
 
-import Types from '@/components/Money/Types.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
@@ -36,7 +35,7 @@ import intervalList from '@/constants/intetvalList';
 import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tabs, Types},
+  components: {Tabs},
 })
 export default class Statistics extends Vue {
   type = '-';
