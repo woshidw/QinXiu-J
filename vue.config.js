c01589e0aeb25069e3827a8cc160168894d6428b
duwei <1069646435@qq.com>
2021-04-08 17:07:20 +0800

    部署网站到 QinXiu-J-website

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
+//eslint-disable-next-line
 const path = require('path')//引入一个path模块
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/QinXiu-J-website/'
+    : '/',
   lintOnSave: false,
+  //eslint-disable-next-line
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
@@ -11,7 +16,8 @@ module.exports = {
       .include.add(dir).end()//只包含icons目录，其他不走.svg规则
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()//不解析成文件
       .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end()
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir)//其他 svg loader 排除 icons目录
   }
