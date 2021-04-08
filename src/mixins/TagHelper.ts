2d1bb1c24a9bbc02b0842c69e82a2ea8b213a28e
duwei <1069646435@qq.com>
2021-04-08 15:55:27 +0800

    修复了一进页面就弹出四个提示的 bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+const map: { [key: string]: string } = {
+  'tag name duplicated': '标签名重复了'
+};
 
 @Component
 export class TagHelper extends Vue {
@@ -7,6 +11,11 @@ export class TagHelper extends Vue {
     const name = window.prompt('请输入标签名');
     if (!name) {return window.alert('标签名不能为空');}
     this.$store.commit('createTag', name);
+    if (this.$store.state.createTagError) {
+      if (this.$store.state.createTagError.message === 'tag name duplicated') {
+        window.alert(map[this.$store.state.createTagError.message] || '未知错误');
+      }
+    }
   }
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,15 +6,11 @@ import router from '@/router';
 
 Vue.use(Vuex);
 
-type RootState = {
-  recordList: RecordItem[];
-  tagList: Tag[];
-  currentTag?: Tag;
-}
 const store = new Vuex.Store({
   state: {
     recordList: [],
     createRecordError: null,
+    createTagError: null,
     tagList: [],
     currentTag: undefined
   } as RootState,
@@ -68,22 +64,23 @@ const store = new Vuex.Store({
     },
     fetchTags(state) {
       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
-      if(!state.tagList || state.tagList.length === 0) {
-        store.commit('createTag', '衣')
-        store.commit('createTag', '食')
-        store.commit('createTag', '住')
-        store.commit('createTag', '行')
+      if (!state.tagList || state.tagList.length === 0) {
+        store.commit('createTag', '衣');
+        store.commit('createTag', '食');
+        store.commit('createTag', '住');
+        store.commit('createTag', '行');
       }
     },
     createTag(state, name: string) {
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标签名重复了');
+        state.createTagError = new Error('tag name duplicated');
+        return;
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
-      window.alert('添加成功');
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
