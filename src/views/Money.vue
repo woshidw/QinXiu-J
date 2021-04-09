0179619059af3be30cba12c55f1772742d0327de
duwei <1069646435@qq.com>
2021-04-09 15:00:04 +0800

    添加了 createdAt 输入框

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,6 +3,12 @@
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Tabs :data-source="recordTypeList"
           :value.sync="record.type"/>
+    <div class="createdAt">
+      <FormItem field-name="日期"
+                type="date"
+                placeholder="在这里输入日期"
+                :value.sync="record.createdAt"/>
+    </div>
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
@@ -32,7 +38,7 @@ export default class Money extends Vue {
   recordTypeList = recordTypeList;
 
   record: RecordItem = {
-    tags: [], notes: '', type: '-', amount: 0
+    tags: [], notes: '', type: '-', amount: 0,createdAt: new Date().toISOString()
   };
 
   created() {
