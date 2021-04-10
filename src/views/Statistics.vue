4f3063fa5f7ca2ab75b1c7a599f834c18d278754
duwei <1069646435@qq.com>
2021-04-10 15:23:01 +0800

    构造30天的数据 （借鉴计数排序）

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -36,7 +36,8 @@ import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
 import Chart from '@/components/Chart.vue';
-
+import _ from 'lodash';
+import day from 'dayjs';
 
 @Component({
   components: {Tabs, Chart},
@@ -48,7 +49,8 @@ export default class Statistics extends Vue {
   }
 
   mounted() {
-    (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+    const div = (this.$refs.chartWrapper as HTMLDivElement);
+    div.scrollLeft = div.scrollWidth;
   }
 
   beautify(string: string) {
@@ -67,20 +69,44 @@ export default class Statistics extends Vue {
       return day.format('YYYY年M月D日');
     }
   }
+  get y(){
+    const today = new Date();
+    const array = [];
+    for (let i = 0; i <= 29; i++) {
+      const dateString = day(today)
+          .subtract(i, 'day').format('YYYY-MM-DD');
+      const found = _.find(this.recordList, {
+        createdAt: dateString
+      });
+      array.push({
+        date: dateString, value: found ? found.amount : 0
+      });
+    }
+
+    array.sort((a, b) =>{
+      if(a.date > b.date){
+        return 1;
+      }else if (a.date === b.date){
+        return 0;
+      }else {
+        return -1;
+      }
+    });
+    return array;
+  }
 
   get x() {
+
+    const keys = this.y.map(item => item.date);
+    const values = this.y.map(item => item.value);
     return {
-      grid:{
+      grid: {
         left: 0,
         right: 0,
       },
       xAxis: {
         type: 'category',
-        data: [
-          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ],
+        data: keys,
         axisTick: {alignWithLabel: true},
         axisLine: {lineStyle: {color: '#666'}}
       },
@@ -92,12 +118,7 @@ export default class Statistics extends Vue {
         symbol: 'circle',
         symbolSize: 12,
         itemStyle: {borderWidth: 1, color: '#666'},
-        data: [
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
-        ],
+        data: values,
         type: 'line'
       }],
       tooltip: {
@@ -134,8 +155,6 @@ export default class Statistics extends Vue {
     }
     result.map(group => {
       group.total = group.items.reduce((sum, item) => {
-        console.log(sum);
-        console.log(item);
         return sum + item.amount;
       }, 0);
     });
@@ -210,7 +229,8 @@ export default class Statistics extends Vue {
 
   &-wrapper {
     overflow: auto;
-    &::-webkit-scrollbar{
+
+    &::-webkit-scrollbar {
       display: none;
     }
   }
